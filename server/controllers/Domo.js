3b2246f657c36b2b4a2a358385d79e7e9800c9db
Thommy Murray Jr <tmm8046@g.rit.edu>
2022-10-31 10:47:16 -0400

    Fixed minor issue

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -15,7 +15,7 @@ const makerPage = (req, res) => {
 
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'Both name age are required!' });
+    return res.status(400).json({ error: 'Both name and age are required!' });
   }
 
   const domoData = {
