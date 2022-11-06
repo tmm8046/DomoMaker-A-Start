c73918ed014d016f313db27b263d3e2fb286ac5e
Thommy Murray Jr <tmm8046@g.rit.edu>
2022-11-06 18:15:15 -0500

    DomoMaker C 0.2

--- client/client.js
+++ client/client.js
@@ -82,7 +82,7 @@ const init = () => {
 
       const username = loginForm.querySelector('#user').value;
       const pass = loginForm.querySelector('#pass').value;
-      const _csrf = signupForm.querySelector('#_csrf').value;
+      const _csrf = loginForm.querySelector('#_csrf').value;
 
       if(!username || !pass) {
         handleError('Username or password is empty!');
@@ -106,7 +106,7 @@ const init = () => {
 
       const name = domoForm.querySelector('#domoName').value;
       const age = domoForm.querySelector('#domoAge').value;
-      const _csrf = signupForm.querySelector('#_csrf').value;
+      const _csrf = domoForm.querySelector('#_csrf').value;
 
       if(!name || !age) {
         handleError('All fields are required!');
